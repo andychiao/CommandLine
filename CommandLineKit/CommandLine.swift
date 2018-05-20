7205bf977c38cefe53f9cb135eb8ae70b9ac76fb
Andy Chiao <andy.chiao@gmail.com>
2018-05-20 14:31:30 +0800

    Fix String.SubSequence to String error.

--- CommandLineKit/CommandLine.swift
+++ CommandLineKit/CommandLine.swift
@@ -312,7 +312,7 @@ public class CommandLine {
       /* Remove attached argument from flag */
       let splitFlag = flagWithArg.split(separator: argumentAttacher, maxSplits: 1)
       let flag = splitFlag[0]
-      let attachedArg: String? = splitFlag.count == 2 ? splitFlag[1] : nil
+      let attachedArg: String? = splitFlag.count == 2 ? String(splitFlag[1]) : nil
 
       var flagMatched = false
       for option in _options where option.flagMatch(String(flag)) {
